01dce5bee6db54363ab5eea5001e7285fbcf98cf
NilAngelats <nilangelatsregincos@gmail.com>
2026-02-20 14:49:41 +0100

    first commit

---
+++ src/fetchs.js
@@ -0,0 +1,30 @@
+/**
+ * Technically, one shared fetch implementation would be enough.
+ * However, the goal here is educational, so it helps to
+ * understand how API authentication works and how to configure
+ * a fetch request independently.
+ */
+
+/**
+ * ========================|
+ * Samara's FETCH          |
+ * ========================|
+ */
+
+/**
+ * ========================|
+ * Patrick's FETCH         |
+ * ========================|
+ */
+
+/**
+ * ========================|
+ * Martin's FETCH          |
+ * ========================|
+ */
+
+/**
+ * ========================|
+ * Nil's FETCH             |
+ * ========================|
+ */
