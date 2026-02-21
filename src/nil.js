e8b6eda25b7bfef0dc6679c7e6d17eb78f646fb6
NilAngelats <nilangelatsregincos@gmail.com>
2026-02-21 10:45:16 +0100

    Without Nils API Key

--- src/nil.js
+++ src/nil.js
@@ -13,8 +13,6 @@
  * Nil's KEY               |
  * ========================|
  */
-const nilKey =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZTVmY2MxYzYyNTU3NjY1MzI0MDQzYzg0YjJiNTZjNiIsIm5iZiI6MTc3MTU5MTY0OS44ODUsInN1YiI6IjY5OTg1N2UxZDZiZTg4YjliODEzYmU2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5IPpNKV4ic4xFpDZWfRu8AKfCZvDczxKkcNWMqRsXRw";
 
 const options = {
   method: "GET",
