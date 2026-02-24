60bb6ac0e1cbc25c9d60f45a625d07f6bf3075b2
Samara <kyzy.samara@gmail.com>
2026-02-24 02:38:42 +0100

    localStorage done

--- src/samara.js
+++ src/samara.js
@@ -23,22 +23,14 @@ import { movieCardLayOut } from "/src/util.js";
  * understand how API authentication works and how to configure
  * a fetch request independently.
  */
-const gridDiv = document.getElementById("thriller-grid");
-function displayMovies(movie){
-    const movieGrid = movieCardLayOut(movie);
-    movieGrid.className = "bg-gray-900 rounded-lg overflow-hidden shadow";
-    const movieImg = document.createElement("img");
-    movieImg.setAttribute("src", "https://image.tmdb.org/t/p/w500" + movie.poster_path);
-    gridDiv.appendChild(movieGrid);
-    movieGrid.appendChild(movieImg);
-}
+
 /**
  * ========================|
  * Samara's FETCH          |
  * ========================|
  */
 import { BASE_URL } from "/src/api.js";
-      
+
 const url = BASE_URL + "/discover/movie?with_genres=53&sort_by=popularity.desc";
 async function fetchMoviesByGenre() {
     try {
@@ -60,17 +52,38 @@ async function fetchMoviesByGenre() {
             console.log(movie.title);
             displayMovies(movie);
         });
-    
+
     } catch (error) {
         console.error(error.message);
     }
 }
-
-
 fetchMoviesByGenre();
+const gridDiv = document.getElementById("thriller-grid");
+function displayMovies(movie) {
+    const movieGrid = movieCardLayOut(movie);
+    movieGrid.addEventListener("click", () => {
+        saveToStorage(movie);
+    });
+
+    movieGrid.className = "bg-gray-900 rounded-lg overflow-hidden shadow";
+    const movieImg = document.createElement("img");
+    movieImg.setAttribute("src", "https://image.tmdb.org/t/p/w500" + movie.poster_path);
+    gridDiv.appendChild(movieGrid);
+    movieGrid.appendChild(movieImg);
+}
 
 /**
  * ========================|
  * Samara's LocalStorage   |
  * ========================|
  */
+function saveToStorage(movie) {
+    const stored = localStorage.getItem("savedMovies");
+    const arr = stored === null ? [] : JSON.parse(stored);
+    const alreadySaved = arr.some(m => m.id === movie.id);
+    if (!alreadySaved) {
+        arr.push(movie);
+        localStorage.setItem("savedMovies", JSON.stringify(arr));
+    }
+
+}
